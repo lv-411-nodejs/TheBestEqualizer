afc1be6748a0dbcdb27f74e6b70e1b04aa827e43
Vishnya <vishnyaostap88@gmail.com>
2019-06-11 15:26:51 +0300

    added empty line

--- src/server/helpers/db.js
+++ src/server/helpers/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/equalizer', { useNewUrlParser: true })
+  .connect('mongodb://localhost/equalizer2', { useNewUrlParser: true })
   .then(() => console.log('MongoDb has started...'))
   .catch((err) => console.log(err));
 
@@ -13,3 +13,5 @@ db.on('error', (err) => {
 db.once('open', () => {
   console.log('we are connected');
 });
+
+module.exports.db = db;
