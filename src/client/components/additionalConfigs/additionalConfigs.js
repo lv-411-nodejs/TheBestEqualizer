d1ee6d1327f1277c96ffddcea808bb052f3ce195
Домьянич Алексей <alexshadow55@gmail.com>
2019-06-13 17:54:46 +0300

    add Modes component

--- src/client/components/additionalConfigs/additionalConfigs.js
+++ src/client/components/additionalConfigs/additionalConfigs.js
@@ -3,31 +3,13 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVolumeUp, faCog } from '@fortawesome/free-solid-svg-icons';
 
-<<<<<<< HEAD
-import classes from './additionalConfigs.css';
-=======
 import './additionalConfigs.css';
->>>>>>> 5d5cd5692fe56d04c7d09a99fe0639e44742ea8f
 
 const volumeIcon = <FontAwesomeIcon icon={faVolumeUp} />;
 const cogIcon = <FontAwesomeIcon icon={faCog} />;
 
 const AdditionalConfigs = () => {
   return (
-<<<<<<< HEAD
-    <section className={ classes.additionalSettingsContainer }>
-      <div className={ classes.volumeElement }>
-        { volumeIcon }
-        <input type='range' /> Volume
-      </div>
-      <div className={ classes.modesElement }>
-        <label className={ classes.switch }>
-          <input type="checkbox"/><span className={ classes.slider }></span>
-        </label>
-        Modes
-      </div>
-      <div className={ classes.settings }>
-=======
     <section className='additionalSettingsContainer'>
       <div className='volumeElement'>
         { volumeIcon }
@@ -40,7 +22,6 @@ const AdditionalConfigs = () => {
         Modes
       </div>
       <div className='settings'>
->>>>>>> 5d5cd5692fe56d04c7d09a99fe0639e44742ea8f
         { cogIcon }
         Settings
       </div>
