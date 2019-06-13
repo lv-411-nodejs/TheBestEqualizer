6196f26b086bc6b1df7d00ed80d68355e45db8e9
Домьянич Алексей <alexshadow55@gmail.com>
2019-06-13 17:23:41 +0300

    change path to Modes

--- src/client/containers/main/main.js
+++ src/client/containers/main/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AdditionalConfigs from '../../components/additionalConfigs/additionalConfigs';
-import Modes from '../../components/modes/modes.js';
+import Modes from '../../components/modes/modes';
 
 const Main = () => {
   return (
