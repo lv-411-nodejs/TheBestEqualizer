8310ed4bbf57631d90c2776c3600b6965dea333c
Russ <kick09111@gmail.com>
2019-06-15 15:16:16 +0300

    created db model of user & added post handler on registration path

--- src/server/controllers/ApiController.js
+++ src/server/controllers/ApiController.js
@@ -1,5 +1,26 @@
+import User from '../models/user';
+
 export default class ApiController {
   static index (req, res) {
     res.json({ version: '0.0.1' });
   }
+
+  static postAddUser (req, res, next) {
+    const firstname = req.body.firstname;
+    const email = req.body.email;
+    const lastname = req.body.lastname;
+    const password = req.body.password;
+    const user = new User({ firstname, lastname, email, password });
+    user
+      .save()
+      .then(result => {
+        res.status(200)
+          .json({
+            result: 'Created User'
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 }
