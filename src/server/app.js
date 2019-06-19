eae21a7f106a3442d81432e06a293e1453269f25
Ruslan Nersezashvili <41145554+Kindest13@users.noreply.github.com>
2019-06-19 09:53:21 +0300

    LVNODCDXI-13 (#17)
    * created db model of user & added post handler on registration path
    
    * changed body request to destructuration

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/api';
+import bodyParser from 'body-parser';
+import dbConnect from './helpers/database';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json({ limit: '2mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
 routes(app);
 
-app.listen(PORT, () => `working on port: ${PORT}`);
+app.listen(PORT, () => {
+  dbConnect();
+});
