2475998bf8a0a5ba0b27db51a3f72e21efeb52ca
StasFedyna <stanislavfedyna@gmail.com>
2019-06-25 12:54:48 +0300

    removed return statement

--- src/client/components/additionalConfigs/settings/settings.js
+++ src/client/components/additionalConfigs/settings/settings.js
@@ -2,13 +2,11 @@ import React from 'react';
 import {settingsIcon} from '../../../assets/icons/icons';
 import './settings.css';
 
-const Settings = () => {
-  return (
+const Settings = () =>  (
     <div className='settingsContainer'>
       { settingsIcon }
       Settings
     </div>
   );
-};
 
 export default Settings;
