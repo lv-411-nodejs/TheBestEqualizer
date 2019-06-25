3d2641a0164e0c2a5fa5f94aeee2277dca2a6e22
StasFedyna <stanislavfedyna@gmail.com>
2019-06-25 14:19:16 +0300

    fixed appheader component

--- src/client/assets/icons/icons.js
+++ src/client/assets/icons/icons.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faVolumeUp, faUpload, faSave, faHeadset, faRss, faTh, faRocket } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faVolumeUp, faUpload, faSave, faHeadset, faRss, faTh, faRocket, faUser } from '@fortawesome/free-solid-svg-icons';
 
 export const settingsIcon = <FontAwesomeIcon icon={faCog} />;
 export const volumeIcon = <FontAwesomeIcon icon={faVolumeUp} />;
@@ -11,3 +11,4 @@ export const headsetIcon = <FontAwesomeIcon icon={faHeadset}/>;
 export const rssIcon = <FontAwesomeIcon icon={faRss}/>;
 export const thIcon = <FontAwesomeIcon icon={faTh}/>;
 export const rocketIcon = <FontAwesomeIcon icon={faRocket}/>;
+export const userIcon = <FontAwesomeIcon icon={faUser}/>;
