55cbe3b38882979a264b7993e08f3a9e9b8ea0e3
StasFedyna <stanislavfedyna@gmail.com>
2019-06-28 19:47:12 +0300

    changed favicon/logo and styles for MainHeaderPage

--- src/client/components/mainPageHeader/mainPageHeader.js
+++ src/client/components/mainPageHeader/mainPageHeader.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { userIcon } from '../../assets/icons/icons';
-import logo from './x4.png';
+import logo from './logo.svg';
 import './mainPageHeader.css';
 
 const MainPageHeader = () => (
   <section className="MainPageHeaderContainer">
-    <div className="AppLogo">
+    <div className="AppLogoContainer">
       <img src={logo} alt="logo" />
       Equalizer
     </div>
     <div className="UserName">
       {userIcon}
-UserName
+      <span>Elon Mask</span>
     </div>
   </section>
 );
