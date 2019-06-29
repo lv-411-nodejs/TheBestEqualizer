4b006ec271c1c6cc1beac57a3627382f7d2b615c
Russ <kick09111@gmail.com>
2019-06-29 18:03:46 +0300

    handled registration and login on server only for db without tokens

--- src/server/controllers/ApiController.js
+++ src/server/controllers/ApiController.js
@@ -1,23 +1,43 @@
 import User from '../models/user';
+import bcrypt from 'bcrypt';
+import { response } from '../helpers/errorHandler';
 
 export default class ApiController {
   static index (req, res) {
     res.json({ version: '0.0.1' });
   }
-
-  static postAddUser (req, res, next) {
-    const { firstname, email, lastname, password } = req.body;
-    const user = new User({ firstname, lastname, email, password });
-    user
-      .save()
-      .then(result => {
-        res.status(200)
-          .json({
-            result: 'Created User'
-          });
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  static postRegistrationHandler (req, res, next) {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email, password });
+    User
+      .findOne({ email })
+      .then(foundUser => foundUser
+        ? response(res, 'User already exists!', 404)
+        : saveUserInDB(res, user))
+      .catch(err => response(res, err.message, 404));
+  };
+  static postLoginHandler (req, res, next) {
+    const { email, password: receivedPassword } = req.body;
+    User
+      .findOne({ email })
+      .then(foundUser => foundUser
+        ? passwordComparison(res, foundUser, receivedPassword)
+        : response(res, 'Login failed', 404))
+      .catch(err => response(res, err.message, 401));
   };
 }
+
+const passwordComparison = (res, foundUser, receivedPassword) => (
+  bcrypt.compare(receivedPassword, foundUser.password)
+    .then(comparisonResult => comparisonResult
+      ? res.status(200).json({ result: 'Logged in' })
+      : response(res, 'Login failed', 404))
+    .catch(err => response(res, err.message, 404))
+);
+
+const saveUserInDB = (res, user) => (
+  user
+    .save()
+    .then(() => res.status(201).json({ result: 'User created' }))
+    .catch(err => response(res, err.message, 422))
+);
