faee6ce56170b52f2b0a03163fa0e7180ac2ae0d
Nadiia Chorna <nadijagopko@gmail.com>
2019-07-01 20:27:14 +0300

    fixed syntax errors

--- src/client/actions/postUserDataAction.js
+++ src/client/actions/postUserDataAction.js
@@ -4,16 +4,17 @@ import { POST_USER_DATA } from './types';
 const baseUrl = 'http://localhost:8080';
 
 const postUserData = (path, newUser, history) => (dispatch) => {
-    axios.post(`${baseUrl}${path}`, newUser)
-    .then(()=>{dispatch({
+  axios.post(`${baseUrl}${path}`, newUser)
+    .then(() => { 
+      dispatch({
         type: POST_USER_DATA,
         status: 'Success authentification',
       })
     })
-    .then(()=>{
+    .then(() => {
       history.push('/main');
     })
-    .catch(({ response: { data: { error } } })=>{
+    .catch(({ response: { data: { error } } }) => {
       dispatch({
         type: POST_USER_DATA,
         status: 'Authentification was failed',
