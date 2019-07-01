aacac4ecaf996c94d22cd4a7dd1bf32e6e0c4267
StasFedyna <stanislavfedyna@gmail.com>
2019-07-01 11:44:50 +0300

    changed svg > .icon class

--- src/client/assets/icons/icons.js
+++ src/client/assets/icons/icons.js
@@ -14,15 +14,15 @@ import {
   faStop, faPlay, faPause,
 } from '@fortawesome/free-solid-svg-icons';
 
-export const settingsIcon = <FontAwesomeIcon icon={faCog} />;
-export const volumeIcon = <FontAwesomeIcon icon={faVolumeUp} />;
-export const uploadIcon = <FontAwesomeIcon icon={faUpload} />;
-export const saveIcon = <FontAwesomeIcon icon={faSave} />;
-export const headsetIcon = <FontAwesomeIcon icon={faHeadset} />;
-export const rssIcon = <FontAwesomeIcon icon={faRss} />;
-export const thIcon = <FontAwesomeIcon icon={faTh} />;
-export const podcastIcon = <FontAwesomeIcon icon={faPodcast} />;
-export const userIcon = <FontAwesomeIcon icon={faUser} />;
-export const stopIcon = <FontAwesomeIcon icon={faStop} />;
-export const playIcon = <FontAwesomeIcon icon={faPlay} />;
-export const pauseIcon = <FontAwesomeIcon icon={faPause} />;
+export const settingsIcon = <FontAwesomeIcon icon={faCog} className="icon" />;
+export const volumeIcon = <FontAwesomeIcon icon={faVolumeUp} className="icon" />;
+export const uploadIcon = <FontAwesomeIcon icon={faUpload} className="icon" />;
+export const saveIcon = <FontAwesomeIcon icon={faSave} className="icon" />;
+export const headsetIcon = <FontAwesomeIcon icon={faHeadset} className="icon" />;
+export const rssIcon = <FontAwesomeIcon icon={faRss} className="icon" />;
+export const thIcon = <FontAwesomeIcon icon={faTh} className="icon" />;
+export const podcastIcon = <FontAwesomeIcon icon={faPodcast} className="icon" />;
+export const userIcon = <FontAwesomeIcon icon={faUser} className="icon" />;
+export const stopIcon = <FontAwesomeIcon icon={faStop} className="icon" />;
+export const playIcon = <FontAwesomeIcon icon={faPlay} className="icon" />;
+export const pauseIcon = <FontAwesomeIcon icon={faPause} className="icon" />;
