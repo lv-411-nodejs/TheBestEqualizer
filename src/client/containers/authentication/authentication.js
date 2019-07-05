81b6d1a3f64a9f1e83019281d3e9029e99bcd357
Russ <kick09111@gmail.com>
2019-07-05 15:45:11 +0300

    changed media query views, changed alter for image, changed submit to input due to logical density, fixed placeholder selector, shortened selector for background

--- src/client/containers/authentication/authentication.js
+++ src/client/containers/authentication/authentication.js
@@ -23,14 +23,14 @@ class Authentication extends Component {
       const message = isMember ? 'Dont have an account? Register!' : 'Already have an account? Login!';
       return (
         <div className="authentication">
-          <img type="image/svg+xml" src={authImage} alt="CooLEqualizer" className="authImage" />
+          <img type="image/svg+xml" src={authImage} alt="Music band" className="authImage" />
           <div className="form-container">
             <h1 className="title">{formTitle}</h1>
             <FormComponent
               isMember={isMember}
               fieldsToRender={fildsToRender}
             />
-            <button type="submit" onClick={this.onLinkClick} className="message">{message}</button>
+            <input type="button" onClick={this.onLinkClick} className="message" value={message} />
           </div>
         </div>
       );
