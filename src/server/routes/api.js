47e5253163c034b4013bd2230da650a886b1daf9
Pidluzhnyy Bohdan <50378554+mr-redFox@users.noreply.github.com>
2019-07-05 12:34:17 +0300

    Lvnodcdxi 26 (#28)
    * add jwt middleware for protected routes
    * added tokens logic and .env for more security
    * configured eslint for server

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,7 +1,7 @@
 import ApiController from '../controllers/ApiController';
 
 export default (app) => {
-  app.get('/api', ApiController.index);
   app.post('/registration', ApiController.postRegistrationHandler);
   app.post('/login', ApiController.postLoginHandler);
+  app.post('/token/refresh', ApiController.refreshTokenHandler);
 };
