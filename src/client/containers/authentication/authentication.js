aa866df31e4b4aafe7b223e8e218692916858700
NadiiaChorna <37384617+NadiiaChorna@users.noreply.github.com>
2019-07-09 19:27:50 +0300

    LVNODCDXI-38 (#32)
    * fixed issue with form fields clearing
    
    * fixed issue with form fields clearing
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * fixed syntax errors
    
    * deleted redundant div
    
    * fixed conflicts

--- src/client/containers/authentication/authentication.js
+++ src/client/containers/authentication/authentication.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
-import FormComponent from '../formComponent/formComponent';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import FormComponent from '../../components/formComponent/formComponent';
 import { formFieldsInfo } from '../../helpers/constants';
+import postUserData from '../../actions/postUserDataAction';
 import authImage from '../../assets/images/authImage.png';
 import './authentication.css';
 
 class Authentication extends Component {
     state = {
       isMember: true,
+      userData: {},
+    };
+
+    onInputChange = ({ target: { name, value } }) => {
+      this.setState(({ userData }) => {
+        userData[name] = value;
+        return userData;
+      });
     };
 
     onLinkClick = () => {
       const { isMember } = this.state;
-      this.setState({ isMember: !isMember });
+      this.setState({
+        isMember: !isMember,
+        userData: {},
+      });
+    };
+
+    onFormSubmit = (submit) => {
+      submit.preventDefault();
+      const { userData: { username, email, password }, isMember } = this.state;
+      const { postUserData: post, history } = this.props;
+      let path;
+      let data;
+
+      if (!isMember) {
+        path = '/registration';
+        data = { username, email, password };
+      } else {
+        path = '/login';
+        data = { email, password };
+      }
+      post(path, data, history);
     };
 
     filterFields = (arr, status) => (status ? arr.filter(el => status === el.isMember) : arr);
 
     render() {
-      const { isMember } = this.state;
+      const { isMember, userData } = this.state;
       const fildsToRender = this.filterFields(formFieldsInfo, isMember);
       const formTitle = isMember ? 'Login' : 'Registration';
       const message = isMember ? 'Dont have an account? Register!' : 'Already have an account? Login!';
@@ -32,8 +64,10 @@ class Authentication extends Component {
           <div className="form-container">
             <h1 className="title">{formTitle}</h1>
             <FormComponent
-              isMember={isMember}
               fieldsToRender={fildsToRender}
+              onInputChange={this.onInputChange}
+              onFormSubmit={this.onFormSubmit}
+              userData={userData}
             />
             <input
               type="button"
@@ -47,4 +81,9 @@ class Authentication extends Component {
     }
 }
 
-export default Authentication;
+Authentication.propTypes = {
+  history: PropTypes.instanceOf(Object).isRequired,
+  postUserData: PropTypes.func.isRequired,
+};
+
+export default connect(null, { postUserData })(withRouter(Authentication));
