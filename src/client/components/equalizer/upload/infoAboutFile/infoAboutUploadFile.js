7971fb4ca6b8b92d590f0522b8cd657cd05a73e1
StasFedyna <stanislavfedyna@gmail.com>
2019-07-09 15:11:02 +0300

    fixed problem after travis

--- src/client/components/equalizer/upload/infoAboutFile/infoAboutUploadFile.js
+++ src/client/components/equalizer/upload/infoAboutFile/infoAboutUploadFile.js
@@ -3,17 +3,14 @@ import PropTypes from 'prop-types';
 import './infoAboutUploadFile.css';
 import { musicIcon } from '../../../../assets/icons/icons';
 
-const InfoAboutTrack = ({ trackname }) => {
-
-  return (
-    <div id="detailstrack">
-      {musicIcon}
-      <span className="TrackName">Track name:</span>
-      {' '}
-      {trackname}
-    </div>
-  );
-};
+const InfoAboutTrack = ({ trackname }) => (
+  <div id="detailstrack">
+    {musicIcon}
+    <span className="TrackName">Track name:</span>
+    {' '}
+    {trackname}
+  </div>
+);
 
 InfoAboutTrack.propTypes = {
   trackname: PropTypes.string,
