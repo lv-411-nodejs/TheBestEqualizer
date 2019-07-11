7ebee6c041ebb8551a25418dc67b9d161ae23e74
OstapVishnya <vishnyaostap88@gmail.com>
2019-07-11 15:21:42 +0300

    add logout

--- src/client/components/logout/logout.js
+++ src/client/components/logout/logout.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import { logoutIcon } from '../../assets/icons/icons';
 import './logout.css';
+import Button from '../button';
 
 class Logout extends Component {
-
     SignOut = (e) => {
-        console.log(e);
+      console.log(e);
     }
-      
-  render() {
-    return (
-      <button type="button" onClick={this.SignOut} className="logout">
-        {this.renderRedirect()}
-        {logoutIcon}
-        Logout
-      </button>
+
+    render() {
+      return (
+        <div>
+          <Button
+            className="ButtonStyleTemplate ButtonLogoutTemplate"
+            type="button"
+            onClick={this.SignOut}
+            icon={logoutIcon}
+            value="Logout"
+          />
+        </div>
       //   <div className="logout">
       //     {logoutIcon}
       //     <a href="/" onClick={this.SignOut}>
       //     Logout
       //     </a>
       //   </div>
-    );
-  }
+      );
+    }
 }
 export default Logout;
