fbb63dedeeb1ce59f43e9d68fee0ee6865de56ff
Nadiia Chorna <nadijagopko@gmail.com>
2019-07-14 18:14:53 +0300

    added schema for saving presets to db

--- src/server/controllers/ApiController.js
+++ src/server/controllers/ApiController.js
@@ -1,13 +1,14 @@
 import User from '../models/user';
+import Effects from '../models/effects';
 import response from '../helpers/errorHandler';
 import { generateTokensPair } from '../helpers/token';
 
 const availableTokens = [];
 
-const saveUserInDB = (res, user) => (
-  user
+const saveDataToDB = (res, data, message) => (
+  data
     .save()
-    .then(() => res.status(201).json({ result: 'User created' }))
+    .then(() => res.status(201).json({ result: message }))
     .catch(err => response(res, err.message, 422))
 );
 
@@ -33,7 +34,7 @@ export default class ApiController {
       .findOne({ email })
       .then(foundUser => (foundUser
         ? response(res, { email: 'User with this email already exists!' }, 404)
-        : saveUserInDB(res, user)))
+        : saveDataToDB(res, user)))
       .catch(err => response(res, err.message, 404));
   }
 
@@ -45,6 +46,27 @@ export default class ApiController {
         ? passwordComparison(res, foundUser, receivedPassword)
         : response(res, 'Login failed', 404)))
       .catch(err => response(res, err.message, 401));
+  }
+
+  static postEffectsHandler(req, res) {
+    const { title, effects } = req.body;
+    const saveEffects = new Effects({ title, effects });
+    Effects
+      .findOne({ title })
+      .then(foundTitle => (foundTitle
+        ? response(res, 'Effect with this title already exists', 404)
+        : saveDataToDB(res, saveEffects, 'The preset have been saved')))
+      .catch(err => response(res, err.message, 404));
+  }
+
+  static getEffectsHandler(req, res) {
+    const { title } = req.body;
+    Effects
+      .findOne({ title })
+      .then(preset => preset
+        ? res.send(preset)
+        : response(res, 'Preset with this title is not found', 404))
+      .catch(err => response(res, err.message, 404));
   }
 
   /**
