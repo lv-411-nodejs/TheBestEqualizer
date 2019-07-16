ffd7a84afe7c401a8d03499f15f916cf3cb12a16
OstapVishnya <vishnyaostap88@gmail.com>
2019-07-16 11:56:31 +0300

    add logout

--- src/client/components/logout/logout.js
+++ src/client/components/logout/logout.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import { logoutIcon } from '../../assets/icons/icons';
 import './logout.css';
 import Button from '../button';
-// import fetchRequest from '../../helpers/fetchRequest';
+import fetchRequest from '../../helpers/fetchRequest';
 
 class Logout extends Component {
-    state ={
-      redirect: false,
-    };
 
     SignOut = () => {
-      // fetchRequest.post('/logout')
-      //   .then(() => {
-      //     localStorage.removeItem('_token');
-      //     this.setState({ redirect: true });
-      //   })
-      //   .catch((error) => {
-      //     console.error(error);
-      //   });
-      this.setState({ redirect: true });
-      console.log(this.state.redirect);
+      fetchRequest.post('/logout')
+        .then(() => {
+          localStorage.removeItem('_token');
+          // this.setState({ redirect: true });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
 
     render() {
@@ -32,9 +27,6 @@ class Logout extends Component {
           onClick={this.SignOut}
         />
       );
-      if (this.state.redirect) {
-        return (<Redirect to="" />);
-      }
       return (
         <div>
           { LogoutButton }
