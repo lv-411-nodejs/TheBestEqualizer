288fb66938d97c01f46d1db9b6068f4d7dd213c1
Oleh <oleh_mushora@ukr.net>
2019-07-16 16:20:58 +0300

    add some code

--- src/client/store/actions/postUserDataAction.spec.js
+++ src/client/store/actions/postUserDataAction.spec.js
@@ -1,7 +1,8 @@
 import configureMockStore from 'redux-mock-store'
 import "babel-polyfill"
 import thunk from 'redux-thunk'
-// import fetchMock from 'fetch-mock'
+import axios from 'axios';
+
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
@@ -20,6 +21,7 @@ import {
 } from './postUserDataAction';
 
 describe('TEST POST ACTIONS', () => {
+ 
   const failUserData = {
     body: {
       email: 'fakeEmail@fake.server',
@@ -35,17 +37,27 @@ describe('TEST POST ACTIONS', () => {
       })
 
 
-      it('CREATE POST USER DATA', () => {      
+      it('CREATE POST USER DATA', async () => {      
 
+        // jest.mock('axios', () => {
+        //   const faikeUserData = {
+        //     body: {
+        //       email: 'fakeEmail@fake.server',
+        //       password: '1111'
+        //     }
+        //   }})
         jest.mock('axios', () => {
-          const failUserData = {
-            body: {
-              email: 'fakeEmail@fake.server',
-              password: '1111'
+          const mockMethods = jest.fn();
+          mockMethods.post = jest.fn().mockImplementation(() => Promise.resolve(            
+            { status: 200, data: {status: "Success authentification"} }));
+            return {
+              __esModule: true,
+              post: mockMethods,
             }
-          }
-  
+          } )   
+       
           const baseUrl = 'http://localhost:8080';
+          
           const response = {
             status: 200,
             body: {
@@ -54,29 +66,24 @@ describe('TEST POST ACTIONS', () => {
               }
             }
           }
-          const myMock = jest.fn();
-  
-          myMock.post = jest.fn((...response) => response);
-          const faikeResult = myMock.post(baseUrl, response, failUserData)
-          return {
-            __esModule: true,
-            default: faikeResult,
-          }
-        })
-  
+          
         const expectedActions = [{
             type: AUTH_START,
           },
-          // {
-          //   type: POST_USER_DATA,
-          //   status: "Success authentification",            
-          // },
+          {
+            status: "Success authentification",
+            type: POST_USER_DATA,                        
+          },
         ]
   
         const store = mockStore({})
-  
-        return store.dispatch(postUserData('/login', failUserData, fakeHistory)).then(() => {
+        const path = '/login';
+        return store.dispatch(postUserData(path, failUserData, fakeHistory)).then(() => {
+          console.log('sdsdf',store.getActions )
           expect(store.getActions()).toEqual(expectedActions)
+
+          // return store.dispatch(postUserData(path, failUserData, fakeHistory)).then(() => {
+          //   expect(store.getActions()).toEqual(expectedActions)
         })
       })
 
@@ -92,7 +99,7 @@ describe('TEST POST ACTIONS', () => {
 
     //     const baseUrl = 'http://localhost:8080';
     //     const response = {
-    //       status: 404,
+    //       status: 200,
     //       body: {
     //         data: {
     //           error: "Login failed"
