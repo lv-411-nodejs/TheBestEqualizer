0d4283d9310e17f9b0d4e871732fcc066d585d81
Домьянич Алексей <alexshadow55@gmail.com>
2019-07-20 18:13:23 +0300

    add test for fomating time

---
+++ src/client/components/trackDuration/trackDuration.test.js
@@ -0,0 +1,7 @@
+import {formatingSongTime} from './trackDuration';
+
+describe('TEST TRACK DURATION', () = > {
+    it('should format seconds in normal type', () => {
+        expect(formatingSongTime(230).toEqual('03:50'))
+    })
+})
