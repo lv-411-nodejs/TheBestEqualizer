f31f6908158703367d896a51bd3e028bac81b228
Домьянич Алексей <alexshadow55@gmail.com>
2019-07-20 19:58:07 +0300

    fix test

--- src/client/components/trackDuration/trackDuration.test.js
+++ src/client/components/trackDuration/trackDuration.test.js
@@ -1,6 +1,6 @@
 import {formatingSongTime} from './trackDuration';
 
-describe('TEST TRACK DURATION', () = > {
+describe('TEST TRACK DURATION', () => {
     it('should format seconds in normal type', () => {
         expect(formatingSongTime(230).toEqual('03:50'))
     })
