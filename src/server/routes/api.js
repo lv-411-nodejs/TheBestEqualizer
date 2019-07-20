7004cbcece7e93ac04b6d8aa1b2a854f15806949
Bohdan Pidluzhnyy <bohdan.pidlugny@gmail.com>
2019-07-20 10:35:47 +0300

    implemented middleware

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,19 +1,13 @@
 // import ApiController from '../controllers/ApiController';
 import { loginUser, registerUser, refreshToken } from '../controllers/authentication';
 import { getEffect } from '../controllers/effects';
+import { middleware } from '../helpers/token';
 
 export default (app) => {
-  // app.post('/registration', ApiController.postRegistrationHandler);
-  // app.post('/login', ApiController.postLoginHandler);
-  // app.post('/effects', ApiController.postEffectsHandler);
-  // app.get('/effects', ApiController.getEffectsHandler);
-  // app.post('/token/refresh', ApiController.refreshTokenHandler);
 
   app.post('/login', loginUser);
   app.post('/registration', registerUser);
   app.post('/token/refresh', refreshToken);
 
-  app.get('/effect', getEffect);
-
-  
+  app.get('/effect', middleware, getEffect);  
 };
