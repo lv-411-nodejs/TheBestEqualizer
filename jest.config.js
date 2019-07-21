d7896b2af12414e0169f173e91b3cef82e51f24a
Домьянич Алексей <alexshadow55@gmail.com>
2019-07-21 14:44:06 +0300

    change test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     verbose: true,
-    moduleDirectories: ["node_modules"], 
-  };
+    moduleDirectories: ["node_modules"],
+    transform: {
+        "^.+\\.[t|j]sx?$": "babel-jest"
+    },
+};
 
 
--- src/client/components/trackDuration/trackDuration.test.js
+++ src/client/components/trackDuration/trackDuration.test.js
@@ -1,7 +1,15 @@
-import {formatingSongTime} from './trackDuration';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import TrackDuration from './trackDuration';
+import Adapter from 'enzyme-adapter-react-16'
+configure({ adapter: new Adapter() })
+
 
 describe('TEST TRACK DURATION', () => {
     it('should format seconds in normal type', () => {
-        expect(formatingSongTime(230).toEqual('03:50'))
+        const wrapper = shallow(<TrackDuration />);
+        expect(wrapper.state('playing')).toBe(false);
+        wrapper.find('button').simulate('click');
+        expect(wrapper.state('playing')).toBe(true);
     })
 })
