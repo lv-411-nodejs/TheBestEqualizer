07996f43822453673f51b08a3cdc16c86b892b8f
skiffua <42241995+skiffua@users.noreply.github.com>
2019-07-24 21:35:15 +0300

    Lvnodcdxi 63 (#57)
    * config jest for react css
    
    * config jest
    
    * lint fix
    
    * correct new line
    
    * lint correct
    
    * add config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     verbose: true,
-    moduleDirectories: ["node_modules"]
-};
+    moduleDirectories: ["node_modules"] ,
+    moduleNameMapper: {
+      "\\.css$": "identity-obj-proxy",
+    },
+    setupFilesAfterEnv: ["./setupTests.js"],
+  };
 
 
