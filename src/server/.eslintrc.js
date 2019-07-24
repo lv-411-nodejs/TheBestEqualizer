4c00bdf4d77388a5a662c60fcb4fe82962a8a1f3
Pidluzhnyy Bohdan <50378554+mr-redFox@users.noreply.github.com>
2019-07-24 21:38:19 +0300

    LVNODCDXI-66 (#55)
    * SUPER HOT FIX resolve problem with redux dev tool ext. Now it work without installed extension and with as before
    
    * a little refator login & register routes
    
    * added effects routes & error classes
    
    * implemented middleware
    
    * adedd redis module
    
    * test
    
    * added redis and refactor refresh toke action
    
    * added redis module and refactore server
    
    * added logout (destroy refresh-token from redis)
    
    * inject
    
    * resolved problems with code && used decorator in redis
    
    * added bracers in if
    
    * fix variables name

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
         "sourceType": "module"
     },
     "rules": {
-		"no-underscore-dangle": ["error", { "allow": ["_id"] }]
+        "no-underscore-dangle": ["error", { "allow": ["_id", "_refresh"] }],
+        "consistent-return": ["error", { "treatUndefinedAsUnspecified": false }]
     }
 };
