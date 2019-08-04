e6e01b5c2255a059ec51d20fc4c8d7ce3fc9c51f
Oleh <oleh_mushora@ukr.net>
2019-08-04 23:16:58 +0300

    lint

--- src/client/test/testUtils.js
+++ src/client/test/testUtils.js
@@ -1,35 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
+
+import rootReducer from '../store/reducers';
+
 const middleware = [thunk];
 
 const enhancers = [applyMiddleware(...middleware)];
 
-import rootReducer from '../store/reducers'
-
-export const storeFactory = (initialState) => {
-    return createStore(rootReducer, initialState,  ...enhancers );
-}
-
-export const mockPizzicato = () => {   
-   return jest.mock('pizzicato', () => {
-        const mockPizzicato = jest.fn();
-        mockPizzicato.context = jest.fn(() => ({}));
-        mockPizzicato.context.createAnalyser = jest.fn(() => 'analyser');
-        mockPizzicato.Effects = jest.fn(()=>{})
-        const effects = ['Delay', 'PingPongDelay', 'DubDelay', 'Distortion', 'Quadrafuzz', 'Flanger',
-                         'Reverb', 'Tremolo',  'StereoPanner', 'Compressor', 'LowPassFilter', 'HighPassFilter', 'RingModulator' ];
-        effects.map((effect)=>{
-          mockPizzicato.Effects[effect] = jest.fn(() => {
-            return function() {
-              return {effect: () => {}};
-            };
-          });
-        })
-        
-        return {
-          __esModule: true,
-          default: mockPizzicato,
-        };
-      });
-      
-}
+export const storeFactory = initialState => createStore(rootReducer, initialState, ...enhancers);
