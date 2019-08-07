4f7e69a64c9fcf165fb1538e560c68b4a2df7991
Oleh <oleh_mushora@ukr.net>
2019-08-07 12:05:48 +0300

    add test snapshot for upload button

--- src/client/components/equalizer/upload/uploadButton.test.js
+++ src/client/components/equalizer/upload/uploadButton.test.js
@@ -1,25 +1,50 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import {
+    storeFactory,
+  } from '../../../test/testUtils';
 
 import UploadButton from './uploadButton';
 
-describe('test infoAboutUploadFile component', () => {
-  const props = {
-    handleInfoFromSound: jest.fn(),
-  };
-
-  let wrapper;
-  beforeEach(()=>{
-    wrapper = shallow(<UploadButton {...props}/>);  
-  })
-
-  it('should render element with `trackname`', () => {
-    
-    expect(wrapper.find('div').text()).toEqual('<FontAwesomeIcon />Track name: fakeName');
+jest.mock('pizzicato', () => {
+    const mockPizzicato = jest.fn();
+    mockPizzicato.context = jest.fn(() => ({}));
+    mockPizzicato.context.createAnalyser = jest.fn(() => 'analyser');
+    mockPizzicato.Effects = jest.fn(() => {});
+    const effects = ['Delay', 'PingPongDelay', 'DubDelay', 'Distortion', 'Quadrafuzz', 'Flanger',
+      'Reverb', 'Tremolo', 'StereoPanner', 'Compressor', 'LowPassFilter', 'HighPassFilter', 'RingModulator',
+    ];
+    effects.forEach((effect, el) => {
+      mockPizzicato.Effects[effect] = jest.fn(() => function Effect() {
+        return {
+          effect: () => (`fakeEffect${el}`),
+        };
+      });
+    });
+    return {
+      __esModule: true,
+      default: mockPizzicato,
+    };
   });
 
+const props ={handleInfoFromSound: jest.fn() } 
+const handleInfoFromSound = jest.fn(); 
+
+const setup = (initialState = {}) => {
+    const store = storeFactory(initialState);
+    const wrapper = shallow(<UploadButton handleInfoFromSound={handleInfoFromSound} store={store}/>).debug();
+    // wrapper.setProps({props: {handleInfoFromSound: jest.fn()}})
+    return wrapper;
+  };
+
+describe('test infoAboutUploadFile component', () => {
+  let wrapper;
+  beforeEach(()=>{
+    wrapper = setup();    
+  }); 
+
   it('should render component properly', () => {
-    
+    console.log(wrapper)
     expect(wrapper).toMatchSnapshot();
   });
 });
